d473bd9fd6c6a8c1d5ba202440d7a06a76d3ff34
test <1342831695@qq.com>
2019-03-27 23:37:22 +0800

    test

---
+++ list/test/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+#define MAXSIZE 100
+typedef struct
+{
+    ElemType *elem;
+    int length;
+}SqlList;
+
+int main()
+{
+    cout << "Hello World!" << endl;
+    return 0;
+}
